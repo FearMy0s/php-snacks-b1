4bda8636985608b06a68c8e7cfb277aa79ad7135
FearMy0s <mirko.giannetta96@gmail.com>
2022-06-22 17:33:10 +0200

    fine es2

--- snack-2.php
+++ snack-2.php
@@ -1,4 +1,9 @@
 <?php
-$User = $_Get['Name']
-$Age = $_Get['Age']
-$Mail = $_Get['Mail']
+    $AccesName = $_GET['name'];
+    $AccesMail = $_GET['mail'];
+    if (strlen($AccesName) > 3 && filter_var($AccesMail, FILTER_VALIDATE_EMAIL)) 
+    {    echo "Accesso Granted";
+    } else {
+        echo "Accesso Denied";
+    }
+?>
